4ee094f6580616ad1a2c7dd18be5a021298e8125
kingbonj <38471159+kingbonj@users.noreply.github.com>
2023-04-26 07:09:06 +0100

    Add files via upload

---
+++ soundfontTest.sh
@@ -0,0 +1,175 @@
+#!/bin/bash
+
+midi_dir=${1:-~/MIDI/}
+
+find_midi_files() {
+    find "$midi_dir" -type f -iname '*.mid' -o -iname '*.midi'
+}
+
+# Function to find SoundFont files
+find_sf2_files() {
+  find /usr/share/sounds/sf2/ -type f -iname "*.sf2"
+}
+
+display_metadata() {
+  # Set color codes for output
+  red='\033[0;31m'
+  cyan='\033[0;36m'
+  magenta='\033[0;35m'
+  black='\033[0;30m'
+  green='\033[0;32m'
+  yellow='\033[0;33m'
+  blue='\033[0;34m'
+  purple='\033[0;35m'
+  white='\033[0;37m'
+  grey='\033[0;90m'    # Grey color code
+  orange='\033[0;33m'  # Orange color code
+  light_yellow='\e[93m'
+  nocolor='\033[0m'
+  
+  # Reset color code
+  nocolor='\033[0m'
+  
+  # Set maximum width for each column
+  col_width=30
+  
+  # Wrap long text using fold command
+  sf2_text=$(echo -e "$2" | fold -s -w $col_width)
+  track_text=$(echo -e "$1" | fold -s -w $col_width)
+  next_track_text=$(echo -e "${3:-None}" | fold -s -w $col_width)
+  
+  # Pad shorter text with spaces to fill the column width
+  sf2_text=$(printf "%-${col_width}s" "$sf2_text")
+  track_text=$(printf "%-${col_width}s" "$track_text")
+  next_track_text=$(printf "%-${col_width}s" "$next_track_text")
+  
+  # Print table headers
+  printf "${blue}%-${col_width}s ${yellow}%-${col_width}s ${magenta}%-${col_width}s ${nocolor}\n" "SoundFont" "Track" "Next Track"
+  printf "${blue}%-${col_width}s ${yellow}%-${col_width}s ${magenta}%-${col_width}s ${nocolor}\n" "---------" "-----" "----------"
+  
+  # Print wrapped and padded text for each row
+  for ((i=1; i<=6; i++)); do
+    sf2_row=$(echo "$sf2_text" | sed "${i}q;d")
+    track_row=$(echo "$track_text" | sed "${i}q;d")
+    next_track_row=$(echo "$next_track_text" | sed "${i}q;d")
+    printf "${cyan}%-${col_width}s ${light_yellow}%-${col_width}s ${grey}%-${col_width}s ${nocolor}\n" "$sf2_row" "$track_row" "$next_track_row"
+  done
+  
+  # Add empty line at the end
+  printf "\n"
+  
+}
+
+
+
+
+
+
+# Function to handle user input
+handle_input() {
+  while true; do
+    read -rsn1 input
+    case "$input" in
+      ".") echo "next";;
+      ",") echo "prev";;
+      "s") echo "sf2";;
+#      "o") echo "save";;
+      "q") echo "quit";;
+      *) continue;;
+    esac
+    break
+  done
+}
+
+# Function to display a menu
+display_menu() {
+  echo "(s)witch soundfont (,)previous (.)next (q)uit"
+}
+
+# Function to save the current track as an mp3
+save_track() {
+  current_track_basename=$(basename "$current_track" .mid)
+  sf2_basename=$(basename "$current_sf2")
+  sf2_basename="${sf2_basename%.*}"
+  output_dir="$(dirname "$0")/Output"
+  output_file="$output_dir/$current_track_basename $sf2_basename.mp3"
+
+  # Kill fluidsynth to ensure that the correct soundfont is used for conversion
+  killall fluidsynth >/dev/null 2>&1
+
+  # Create Output folder if it doesn't exist
+  if [ ! -d "$output_dir" ]; then
+    mkdir "$output_dir"
+  fi
+
+  echo "Saving track as $output_file..."
+  sf2_current=$(basename "$current_sf2")
+  timidity "$current_track" -Ow -o - | lame - "$output_file"
+  echo "Track saved to $output_dir."
+}
+
+# Function to clean up when the script exits
+cleanup() {
+  clear
+  echo "Stopping running processes..."
+  killall fluidsynth
+  if pidof pulseaudio; then
+    pulseaudio -k
+  fi
+  echo "Done."
+}
+
+
+# Set up trap to call cleanup function when the script exits
+trap cleanup EXIT
+
+shuffled_midi_files=()
+while IFS= read -r -d '' file; do
+  shuffled_midi_files+=("$file")
+done < <(find "$midi_dir" -type f \( -iname "*.mid" -o -iname "*.midi" \) -print0 | shuf -z)
+
+sf2_files=($(find_sf2_files))
+current_track_index=0
+current_sf2_index=0
+
+while true; do
+  current_track="${shuffled_midi_files[$current_track_index]}"
+  next_track="${shuffled_midi_files[$((current_track_index + 1))]}"
+  current_sf2="${sf2_files[$current_sf2_index]}"
+  
+  clear
+  
+  echo -e "MIDI Soundfont Testing Program v1.0"
+  echo ""
+  display_metadata "$current_track" "$current_sf2" "$next_track"
+  display_menu
+
+  fluidsynth -a pulseaudio -m alsa_seq -l -i "$current_sf2" "$current_track" >/dev/null 2>&1 &
+
+  fluidsynth_pid=$!
+
+  while true; do
+    input=$(handle_input)
+
+    if [[ "$input" == "next" ]]; then
+      kill $fluidsynth_pid
+      current_track_index=$((current_track_index + 1))
+      break
+    elif [[ "$input" == "prev" ]]; then
+      kill $fluidsynth_pid
+      current_track_index=$((current_track_index - 1))
+      break
+    elif [[ "$input" == "quit" ]]; then
+      cleanup
+      exit 0
+    #elif [[ "$input" == "save" ]]; then
+    #  save_track
+    #  break
+    elif [[ "$input" == "sf2" ]]; then
+      kill $fluidsynth_pid
+      current_sf2_index=$((current_sf2_index + 1))
+      current_sf2_index=$((current_sf2_index % ${#sf2_files[@]}))
+      break
+    fi
+  done
+done
